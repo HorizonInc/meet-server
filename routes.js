5a6ac636e85f8c8b55164fc2f52ee2dc0e72831d
Jesse Davda <jessedavda@gmail.com>
2017-07-18 21:48:17 +0100

    started working on the login and the main views

--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
 module.exports = (app) => {
+
     app.get("/", (req, res) => {
         res.render("index");
+    });
+
+    app.get("/login", (req, res) => {
+        res.render("login");
     });
 }
---
+++ static/javascript/script.js
@@ -0,0 +1,8 @@
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map") , {
+        center: {lat: 51.5074, lng: 0.1278},
+        zoom: 4
+    });
+}
