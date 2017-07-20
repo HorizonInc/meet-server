8aa8f404300c8ac7953cee8658fce36466423d71
Mair Davda <mairdavda@mairs-mbp.home>
2017-07-20 14:06:44 +0100

    split the css files up and starting to work on the login page

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const redis = require("redis");
 
 const app = express();
 
+const client = redis.createClient();
+
+client.on("connect", (err) => {
+	if(err != undefined)
+		console.log(err);
+})
+
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.use(express.static(__dirname + "/static"));
 
