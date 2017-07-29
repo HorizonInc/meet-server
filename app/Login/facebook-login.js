e0ecf9f8152057c825d46851322c90341b55ec0b
Jesse <stratosphere1243@gmail.com>
2017-07-30 00:29:45 +0100

    still getting the weird authentication error

--- app/Login/facebook-login.js
+++ app/Login/facebook-login.js
@@ -1,6 +1,6 @@
 const facebookAuth = {
-    'clientID' : '129',
-    'clientSecret' : '18e',
+    'clientID' : '129564380981950',
+    'clientSecret' : '18e28b0e8693fab6132a4771fc5da22a',
     'callbackURL' : 'http://localhost:8080/fbCode'
 }
 
@@ -23,20 +23,21 @@ module.exports = (app) => {
         //console.log(code);
         //console.log(err);
 
-        console.log(facebookAuth.clientID);
-        console.log(facebookAuth.clientSecret);
+        //console.log(facebookAuth.clientID);
+        //console.log(facebookAuth.clientSecret);
 
-        //if(err != undefined) {
-        //    console.log("There was an error loggin in with facebook: " + err + " " + errdesc);
-        //} else if(err ="undefined") {
+        if(err != undefined) {
+            console.log("There was an error logging in with facebook: " + err + " " + errdesc);
+        } else if(err ="undefined") {
               request.get(`https://graph.facebook.com/v2.10/oauth/access_token?
                            &redirect_uri=http://localhost:8080/
                            &client_id=${facebookAuth.clientID}
                            &client_secret=${facebookAuth.clientSecret}
                            &code=${code}`, (err, res, body) => {
                                console.log(body);
-                           })
-        //}
+                               access_token = body.access_token;
+                           });
+        }
 
 
     });
