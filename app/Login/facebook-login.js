c02ad8844590dafda7a017d1fab6265b7146ea4a
Jesse Davda <stratosphere1243@gmail.com>
2017-07-30 00:09:03 +0100

    getting weird 'cannot validate client secret' errors from the facebook api

--- app/Login/facebook-login.js
+++ app/Login/facebook-login.js
@@ -1,14 +1,15 @@
 const facebookAuth = {
-    'clientID' : '123',
-    'clientSecret' : 'dbe',
+    'clientID' : '129',
+    'clientSecret' : '18e',
     'callbackURL' : 'http://localhost:8080/fbCode'
 }
 
 module.exports = (app) => {
     const querystring = require('querystring');
+    const request = require('request');
 
     app.get("/fbLogin", (req, res) => {
-        res.redirect(`https://www.facebook.com/v2.30/dialog/oauth?` +
+        res.redirect(`https://www.facebook.com/v2.10/dialog/oauth?` +
                       `client_id=${facebookAuth.clientID}` +
                       `&redirect_uri=${facebookAuth.callbackURL}`);
     });
@@ -17,20 +18,25 @@ module.exports = (app) => {
         var code = req.query.code;
         var err = req.query.error;
         var errdesc = req.query.error_description;
+        var access_token;
 
-        console.log(code);
-        console.log(err);
+        //console.log(code);
+        //console.log(err);
 
-        if(err != undefined) {
-            console.log("There was an error loggin in with facebook: " + err + " " + errdesc);
-        } else if(err ="undefined") {
-            app.get(`https://graph.facebook.com/v2.30/oauth/access_token?
-                     client_id=${facebookAuth.clientID}
-                     &client_secret=${facebookAuth.clientSecret}
-                     &code=${code}`, (req, res) => {
-                         console.log(res.body);
-                     });
-        }
+        console.log(facebookAuth.clientID);
+        console.log(facebookAuth.clientSecret);
+
+        //if(err != undefined) {
+        //    console.log("There was an error loggin in with facebook: " + err + " " + errdesc);
+        //} else if(err ="undefined") {
+              request.get(`https://graph.facebook.com/v2.10/oauth/access_token?
+                           &redirect_uri=http://localhost:8080/
+                           &client_id=${facebookAuth.clientID}
+                           &client_secret=${facebookAuth.clientSecret}
+                           &code=${code}`, (err, res, body) => {
+                               console.log(body);
+                           })
+        //}
 
 
     });
