fdc6df8c67d8e4a3f7bd7b9ae84cf4ca1766585b
JesseDavda <jessepilot@live.com>
2018-05-17 22:53:49 +0100

    img

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,10 @@
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
+        res.redirect('/login');
+    });
+
+    app.get('/login', (req, res) => {
         res.render('login');
     });
 
