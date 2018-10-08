6bc6dc00d9df953d5529a8eb9d1cdc321e4a00a8
JesseDavda <jessedavda@gmail.com>
2018-10-08 16:43:48 +0100

    started work on sesions for the API, trying to figure out how cookies work

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 'use strict';
-
 require('dotenv').load();
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+var sessionStore = new MongoDBStore({
+    uri: "mongodb://localhost:27017/Meet",
+    collection: "sessions"
+});
+
+let expiry = (96 * 60 * 60 * 1000);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'V8W060£6F0#DF6DSNM67',
+    saveUninitialized: false,
+    resave: false,
+    store: sessionStore,
+    cookie: { maxAge: expiry }
+}));
 
 require('./db.js');
 
