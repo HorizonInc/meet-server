b72241471ca1405fd481c519fa22fc737dac3437
JesseDavda <jessepilot@live.com>
2018-11-05 19:41:28 +0000

    more patches

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
-'use strict';
-const mongoose = require('mongoose');
+"use strict";
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-mongoose.promise = require('bluebird');
-mongoose.connect('mongodb://localhost:27017/Meet', { useNewUrlParser: true});
+mongoose.promise = require("bluebird");
+mongoose.connect("mongodb://localhost:27017/Meet", { useNewUrlParser: true});
 
 var userDataSchema = new Schema({
-    name: String,
-    meetId: String,
-    pictureURL: String,
-    invites: Array,
+	name: String,
+	meetId: String,
+	pictureURL: String,
+	invites: Array,
 });
 
 var groupDataSchema = new Schema({
-    group_admin_id: String,
-    accepted_members: Array,
-    invited_members: Array,
+	group_admin_id: String,
+	accepted_members: Array,
+	invited_members: Array,
 });
 
 var UserData = mongoose.model('UserData', userDataSchema, 'UserData');  // eslint-disable-line
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-'use strict';
-require('dotenv').load();
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+"use strict";
+require("dotenv").load();
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const app = express();
 
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 var sessionStore = new MongoDBStore({
-    uri: "mongodb://localhost:27017/Meet",
-    collection: "sessions"
+	uri: "mongodb://localhost:27017/Meet",
+	collection: "sessions"
 });
 
 let expiry = (96 * 60 * 60 * 1000);
@@ -20,23 +20,23 @@ let expiry = (96 * 60 * 60 * 1000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
-    secret: 'V8W060£6F0#DF6DSNM67',
-    saveUninitialized: false,
-    resave: false,
-    store: sessionStore,
-    cookie: { maxAge: expiry }
+	secret: "V8W060£6F0#DF6DSNM67",
+	saveUninitialized: false,
+	resave: false,
+	store: sessionStore,
+	cookie: { maxAge: expiry }
 }));
 
-require('./db.js');
+require("./db.js");
 
-require('./lib/Login/facebook.js')(app);
+require("./lib/Login/facebook.js")(app);
 
-require('./lib/genGroup.js')(app);
-require('./lib/groupInvites.js')(app);
+require("./lib/genGroup.js")(app);
+require("./lib/groupInvites.js")(app);
 
 // loading the web socket connection module
-require('./lib/socketConnection.js')(app, io);
+require("./lib/socketConnection.js")(app, io);
 
 var port = 8080 || process.env.PORT;
 server.listen(port);
-console.log('server listening on port ' + port);
+console.log("server listening on port " + port);
